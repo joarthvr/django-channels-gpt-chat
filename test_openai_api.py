63fb8ed0091c1abb28f2b6026ad64b2a5cbf38b7
arthur <iii1003@naver.com>
2024-02-05 22:58:30 +0900

    Initial commit settings

---
+++ test_openai_api.py
@@ -0,0 +1,30 @@
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+    engine = "gpt-3.5-turbo-instruct",
+    prompt = """
+    Fix grammer errors:
+    -I is a boy
+    -You is a girl""".strip(),
+)
+
+print(response)
+
+print(response.choices[0].text.strip())
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "당신은 지식이 풍부한 도우미입니다."},
+        {"role": "user", "content": "세계에서 가장 큰 도시는 어디인가요?"}
+    ],
+)
+
+print(response)
+print(response["choices"][0]["message"]["content"])
